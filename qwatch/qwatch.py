6a3a9e7fd905bb95273ab82777897e7760473063
RAG-home <rgilmore@umc.edu>
2018-03-02 23:06:33 -0600

    Added a filter function and outlined waiting and plotting.

--- qwatch/qwatch.py
+++ qwatch/qwatch.py
@@ -16,7 +16,7 @@ import re
 @click.option('--data_filename', default="qwatch_data")
 @click.option('-plot', is_flag=True, default=False)
 @click.option('--notify', nargs=2)
-def qwatch(user, job, watch, outfile, save_data, data_filename, plot, notify):
+def qwatch(user, job, watch, outfile, data_filename, plot, notify):
     cmd = "qstat -f"
     qstat = subprocess.Popen(cmd, stderr=subprocess.PIPE,
                                stdout=subprocess.PIPE, shell=True,
@@ -34,27 +34,44 @@ def qwatch(user, job, watch, outfile, save_data, data_filename, plot, notify):
         tf.writelines(out)
         job_dict = qstat_parser(tf)
         print(job_dict)
-        kept_jobs = []
-        for j in job_dict.keys():
-            if user:
-                if user in job_dict[j]["Job_Owner"]:
-                    kept_jobs.append(j)
-            if job:
-                if job in job_dict[j]["Job_Name"]:
-                    kept_jobs.append(j)
-                elif job in j:
-                    kept_jobs.append(j)
-            if not job and not user:
-                kept_jobs.append(j)
-
-        kept_jobs = list(set(kept_jobs))
-        kept_dict = dict((k, job_dict[k]) for k in kept_jobs)
+        kept_jobs, kept_dict = filter_jobs(job_dict, user=user, job=job)
         tf.truncate()
         yaml.dump(kept_dict, stream=tf)
-        if watch:
-            for job in kept_jobs:
-                pass
-                # Monitor jobs here
+    if watch:
+        df_list = []
+        for job in kept_jobs:
+            data_filename = "%s_%s.csv" % (data_filename, job.split(".")[0])
+            df_list.append(data_filename)
+            subprocess.Popen(["python3.6 watcher --job %s --data_filename %s" % (job, data_filename)])
+    if plot:
+        # Wait for data file and then plot and/or notify.
+        for job, df in kept_jobs, df_list:
+            if plot and notify:
+                subprocess.Popen(["python3.6 waiter --job %s --plot %s --notify %s" % (job, df, notify)])
+            elif plot:
+                subprocess.Popen(["python3.6 waiter --job %s --plot %s" % (job, df)])
+    elif notify:
+        for job in kept_jobs:
+            subprocess.Popen(["python3.6 waiter --job %s --notify %s" % (job, notify)])
+
+
+def filter_jobs(job_dict, user, job):
+    kept_jobs = []
+    for j in job_dict.keys():
+        if user:
+            if user in job_dict[j]["Job_Owner"]:
+                kept_jobs.append(j)
+        if job:
+            if job in job_dict[j]["Job_Name"]:
+                kept_jobs.append(j)
+            elif job in j:
+                kept_jobs.append(j)
+        if not job and not user:
+            kept_jobs.append(j)
+
+    kept_jobs = list(set(kept_jobs))
+    kept_dict = dict((k, job_dict[k]) for k in kept_jobs)
+    return kept_jobs, kept_dict
 
 
 def qstat_parser(qstat_stream):
@@ -104,118 +121,7 @@ def qstat_parser(qstat_stream):
                 value = Path(value)
             temp_dict[key] = value
         mast_dict[main_key]["Variable_List"] = temp_dict
-    return(mast_dict)
-
-
-def wait_on_job_completion(self, job_id):
-    """Use Qstat to monitor your job."""
-    # TODO Allow either slack notifications or email or text.
-    qwatch = Qstat().watch(job_id)
-    if qwatch == 'Job id not found.':
-        self.sgejob_log.info('%s has finished.' % job_id)
-        sleep(30)
-    elif qwatch == 'Waiting for %s to start running.' % job_id:
-        self.sgejob_log.info('%s is queued to run.' % job_id)
-        self.sgejob_log.info('Waiting for %s to start.' % job_id)
-        sleep(30)
-        self.wait_on_job_completion(job_id)
-    elif qwatch == 'Waiting for %s to finish running.' % job_id:
-        self.sgejob_log.info('%s is running.' % job_id)
-        self.sgejob_log.info('Waiting for %s to finish.' % job_id)
-        sleep(30)
-        self.wait_on_job_completion(job_id)
-    else:
-        self.wait_on_job_completion(job_id)
-
-
-"""Access qstat information about SGE jobs."""
-class Qstat(object):
-    def __init__(self):
-        """Initialize class."""
-        _username = getpass.getuser()
-        self.username = _username
-        self.split_regex = re.compile(r'\s+')
-
-    def qstatinfo(self, qstat_path='qstat'):
-        """Retrieve qstat output."""
-        try:
-            qstatinfo = subprocess.check_output([qstat_path])
-        except subprocess.CalledProcessError as cpe:
-            return_code = 'qstat returncode: %s' % cpe.returncode
-            std_error = 'qstat standard output: %s' % cpe.stderr
-            print(return_code + '\n' + std_error)
-        except FileNotFoundError:
-            raise FileNotFoundError('qstat is not on your machine.')
-
-        jobs = self._output_parser(qstatinfo)
-
-        return jobs
-
-    def _output_parser(self, output):
-        """Parse output from qstat pbs commandline program.
-        Returns a list of dictionaries for each job.
-        """
-        lines = output.decode('utf-8').split('\n')
-        del lines[:5]
-        jobs = []
-        for line in lines:
-            els = self.split_regex.split(line)
-            try:
-                j = {"job_id": els[0], "name": els[1], "user": els[2], "elapsed_time": els[3],
-                     "status": els[4], "queue": els[5]}
-                jobs.append(j)
-
-            except IndexError:
-                pass
-
-        return jobs
-
-    def all_job_ids(self):
-        """Retrieve a list of all jobs running or queued."""
-        jobs = self.qstatinfo()
-        ids = [j['job_id'] for j in jobs]
-        return ids
-
-    def all_running_jobs(self):
-        """Retrieve a list of running jobs."""
-        jobs = self.qstatinfo()
-        ids = [j['job_id'] for j in jobs if j['status'] == 'R']
-        return ids
-
-    def all_queued_jobs(self):
-        """Retrieve a list of queued jobs."""
-        jobs = self.qstatinfo()
-        ids = [j['job_id'] for j in jobs if j['status'] == 'Q']
-        return ids
-
-    def myjobs(self):
-        """Retrieve a list of all the current user's jobs."""
-        jobs = self.qstatinfo()
-        ids = [j['job_id'] for j in jobs if j['user'] == self.username]
-        if len(ids) < 1:
-            return 'You have no jobs running or queued.'
-        else:
-            rids = [j['job_id'] for j in jobs if j['user'] == self.username
-                    and j['status'] == 'R']
-            qids = [j['job_id'] for j in jobs if j['user'] == self.username
-                    and j['status'] == 'Q']
-            return 'Running jobs: %s\nQueued jobs: %s' % (rids, qids)
-
-    def watch(self, job_id):
-        """Wait until a job or list of jobs finishes and get updates."""
-        jobs = self.qstatinfo()
-        rids = [j['job_id'] for j in jobs if j['user'] == self.username
-                and j['status'] == 'R']
-        qids = [j['job_id'] for j in jobs if j['user'] == self.username
-                and j['status'] == 'Q']
-        if job_id in qids:
-            yield 'Waiting for %s to start running.' % job_id
-            self.watch(job_id)
-        elif job_id in rids:
-            yield 'Waiting for %s to finish running.' % job_id
-            self.watch(job_id)
-        else:
-            return 'Job id not found.'
+    return mast_dict
 
 
 if __name__ == '__main__':
