d57bb3480f27029cabaeb7771220bfb767e2ab61
rgilmore <rgilmore@umc.edu>
2018-03-29 19:20:09 -0500

    Updated bugs.

--- qwatch/waiter.py
+++ qwatch/waiter.py
@@ -20,26 +20,31 @@ with open(yfile, 'r') as yf:
 
 async def _async_watch(job_id, directory, sleeper=120, **kwargs):
     """Wait until a job or list of jobs finishes and get updates."""
-    directory = directory / Path(job_id)
+    if job_id != directory.stem:
+        directory = directory / Path(job_id)
     watch_one = qwatch.Qwaiter(jobs=[job_id], directory=directory, **kwargs)
     job_dict = watch_one.full_workflow(watch=True, parse=True, process=True, data=True, metadata=False)
-    md = watch_one.get_metadata(watch_flag=True, data_frame=True)
-    ev = watch_one.get_pbs_env(watch_flag=True, data_frame=True)
-    tm = watch_one.get_time(watch_flag=True, data_frame=True)
-    rs = watch_one.get_resources(watch_flag=True, data_frame=True)
-    watch_one.update_csv(file=watch_one.metadata_filename, data=md)
-    watch_one.update_csv(file=watch_one.vl_metadata_filename, data=ev)
-    watch_one.update_csv(file=watch_one.time_metadata_filename, data=tm)
-    watch_one.update_csv(file=watch_one.resource_metadata_filename, data=rs)
 
-    if job_dict[job_id]['job_state'] == 'Q':
-        #yield 'Waiting for %s to start running.' % job_id
-        sleep(sleeper)
-        await _async_watch(job_id, directory, sleeper, **kwargs)
-    elif job_dict[job_id]['job_state'] == 'R':
-        #yield 'Waiting for %s to finish running.' % job_id
-        sleep(sleeper)
-        await _async_watch(job_id, directory, sleeper, **kwargs)
+    if job_dict:
+        md = watch_one.get_metadata(watch_flag=True, data_frame=True)
+        ev = watch_one.get_pbs_env(watch_flag=True, data_frame=True)
+        tm = watch_one.get_time(watch_flag=True, data_frame=True)
+        rs = watch_one.get_resources(watch_flag=True, data_frame=True)
+        watch_one.update_csv(file=watch_one.metadata_filename, data=md)
+        watch_one.update_csv(file=watch_one.vl_metadata_filename, data=ev)
+        watch_one.update_csv(file=watch_one.time_metadata_filename, data=tm)
+        watch_one.update_csv(file=watch_one.resource_metadata_filename, data=rs)
+        print(f"Updated qstat data for {job_id}")
+
+        if job_dict[job_id]['job_state'] == 'Q':
+            #yield 'Waiting for %s to start running.' % job_id
+            sleep(sleeper)
+            await _async_watch(job_id, directory, sleeper, **kwargs)
+        elif job_dict[job_id]['job_state'] == 'R':
+            #yield 'Waiting for %s to finish running.' % job_id
+            sleep(sleeper)
+            await _async_watch(job_id, directory, sleeper, **kwargs)
+
     return f'Finished {job_id}'
 
 
