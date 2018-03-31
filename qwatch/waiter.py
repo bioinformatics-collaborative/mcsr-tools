836b0b8d9eb27fd2450af873eed176e9ac57be96
rgilmore <rgilmore@umc.edu>
2018-03-30 19:23:09 -0500

    Updated data retrieval and cleaned garbage.

--- qwatch/waiter.py
+++ qwatch/waiter.py
@@ -27,20 +27,16 @@ async def _async_watch(job_id, directory, sleeper=120, **kwargs):
     if job_dict:
         md = watch_one.get_metadata(data_frame=True)
         ev = watch_one.get_pbs_env(data_frame=True)
-        tm = watch_one.get_time(data_frame=True)
-        rs = watch_one.get_resources(data_frame=True)
+        pl = watch_one.get_plot_data(data_frame=True)
         watch_one.update_csv(file=watch_one.metadata_filename, data=md)
         watch_one.update_csv(file=watch_one.vl_metadata_filename, data=ev)
-        watch_one.update_csv(file=watch_one.time_metadata_filename, data=tm)
-        watch_one.update_csv(file=watch_one.resource_metadata_filename, data=rs)
+        watch_one.update_csv(file=watch_one.plot_metadata_filename, data=pl)
         print(f"Updated qstat data for {job_id}")
 
         if job_dict[job_id]['job_state'] == 'Q':
-            #yield 'Waiting for %s to start running.' % job_id
             sleep(sleeper)
             await _async_watch(job_id, directory, sleeper, **kwargs)
         elif job_dict[job_id]['job_state'] == 'R':
-            #yield 'Waiting for %s to finish running.' % job_id
             sleep(sleeper)
             await _async_watch(job_id, directory, sleeper, **kwargs)
 
@@ -49,9 +45,6 @@ async def _async_watch(job_id, directory, sleeper=120, **kwargs):
 
 async def _async_watch_jobs(jobs, sleeper, home_dir, **kwargs):
 
-    # with tempfile.TemporaryDirectory() as tempdir:
-    print(f'async_jobs:{jobs}')
-    print(f'async_kwargs:{kwargs}')
     tasks = [_async_watch(job_id=job, directory=home_dir/Path(job), sleeper=sleeper, **kwargs) for job in jobs]
     return await asyncio.gather(*tasks)
 
