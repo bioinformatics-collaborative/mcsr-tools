5e0563df088a02bfc85d2bece8c29349eaf6c7ed
rgilmore <rgilmore@umc.edu>
2018-04-05 19:39:39 -0500

    Updated for new sleeper parameter and data/info output files.

--- qwatch/waiter.py
+++ qwatch/waiter.py
@@ -19,40 +19,40 @@ with open(yfile, 'r') as yf:
     _kwargs = yaml.load(yf)
 
 
-async def _async_watch(job_id, directory, sleeper=120, python_datetime=None, **kwargs):
-    """Wait until a job or list of jobs finishes and get updates."""
+async def _async_watch(job_id, directory, python_datetime=None, first_time=True, **kwargs):
+    """Wait until a list of jobs finishes and get updates."""
 
     watch_one = qwatch.Qwaiter(jobs=[job_id], directory=directory,  watch=True, users=None, **kwargs)
     job_dict = watch_one.full_workflow(parse=True, process=True, data=True, metadata=False)
 
     if job_dict:
-        md = watch_one.get_metadata(data_frame=True, python_datetime=python_datetime)
-        ev = watch_one.get_pbs_env(data_frame=True, python_datetime=python_datetime)
-        pl = watch_one.get_plot_data(data_frame=True, python_datetime=python_datetime)
-        watch_one.update_csv(file=watch_one.metadata_filename, data=md)
-        watch_one.update_csv(file=watch_one.pbs_env_filename, data=ev)
-        watch_one.update_csv(file=watch_one.plot_md_filename, data=pl)
+        data = watch_one.get_data(data_frame=True, python_datetime=python_datetime)
+        watch_one.update_csv(file=watch_one.data_filename, data=data)
         print(f"Updated qstat data for {job_id}")
 
         if job_dict[job_id]['job_state'] == 'Q':
-            await asyncio.sleep(sleeper)
-            await _async_watch(job_id, directory, sleeper, datetime.now(), **kwargs)
+            await asyncio.sleep(watch_one.sleeper)
+            await _async_watch(job_id, directory, datetime.now(), first_time=True **kwargs)
         elif job_dict[job_id]['job_state'] == 'R':
-            await asyncio.sleep(sleeper)
-            await _async_watch(job_id, directory, sleeper, datetime.now(), **kwargs)
+            # Create the static data file on the first instance of a running job
+            if first_time:
+                info = watch_one.get_info(data_frame=True, python_datetime=python_datetime)
+                watch_one.update_csv(file=watch_one.info_filename, data=info)
+            await asyncio.sleep(watch_one.sleeper)
+            await _async_watch(job_id, directory, datetime.now(), first_time=False, **kwargs)
 
     return f'Finished {job_id}'
 
 
-def get_watcher_tasks(jobs, sleeper, home_dir, **kwargs):
+def get_watcher_tasks(jobs, home_dir, **kwargs):
     python_datetime = datetime.now()
-    tasks = [asyncio.ensure_future(_async_watch(job_id=job, directory=home_dir/Path(job), sleeper=sleeper,
+    tasks = [asyncio.ensure_future(_async_watch(job_id=job, directory=home_dir/Path(job),
                                                 python_datetime=python_datetime, **kwargs)) for job in jobs]
     return tasks
 
 
 ioloop = asyncio.get_event_loop()
-tasks = get_watcher_tasks(jobs=_kwargs["jobs"], sleeper=_kwargs["sleeper"],
+tasks = get_watcher_tasks(jobs=_kwargs["jobs"],
                           home_dir=_kwargs["directory"], **_kwargs["kwargs"])
 ioloop.run_until_complete(asyncio.wait(tasks))
 ioloop.close()
