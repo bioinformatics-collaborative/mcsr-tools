0aa838e129c4a778c9a6bf897f7e817811f7956a
rgilmore <rgilmore@umc.edu>
2018-04-11 18:16:47 -0500

    Fixed qwatch/__init__.py functionality.

--- qwatch/qwatch/__init__.py
+++ qwatch/qwatch/__init__.py
@@ -1,5 +1,9 @@
 __version__ = '0.1.0'
-from .qwatch import Qwatch
+from .core import Qwatch, NotRequiredIf
+from .cli import qwatch
 __all__ = (
     "Qwatch",
+    "NotRequiredIf",
+    "utils",
+    "qwatch",
            )
