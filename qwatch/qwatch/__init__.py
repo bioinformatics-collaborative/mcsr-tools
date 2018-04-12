6126b400efb253f55d017e39f42efa17e6cae731
rgilmore <rgilmore@umc.edu>
2018-04-12 17:57:12 -0500

    Moved custom NotRequiredIf(click.Option) to the __init__.py file.

--- qwatch/qwatch/__init__.py
+++ qwatch/qwatch/__init__.py
@@ -1,6 +1,15 @@
 __version__ = '0.1.0'
-from .core import Qwatch, NotRequiredIf
+from .core import Qwatch
 from .cli import qwatch
+
+
+__all__ = (
+    "Qwatch",
+    "NotRequiredIf",
+    "utils",
+    "qwatch",
+    "_setup_yaml",
+           )
 
 
 def represent_dictionary_order(cls, dict_data):
@@ -11,10 +20,30 @@ def _setup_yaml():
     """ https://stackoverflow.com/a/8661021 """
     yaml.add_representer(OrderedDict, represent_dictionary_order)
 
-__all__ = (
-    "Qwatch",
-    "NotRequiredIf",
-    "utils",
-    "qwatch",
-    "_setup_yaml",
-           )
+
+# Borrowed from the discussion in the link below:
+# https://stackoverflow.com/questions/44247099/click-command-line-interfaces-make-options-required-if-other-optional-option-is
+class NotRequiredIf(click.Option):
+    def __init__(self, *args, **kwargs):
+        self.not_required_if = kwargs.pop('not_required_if')
+        assert self.not_required_if, "'not_required_if' parameter required"
+        kwargs['help'] = (kwargs.get('help', '') +
+                          ' NOTE: This argument is mutually exclusive with %s.' %
+                          self.not_required_if
+                          ).strip()
+        super(NotRequiredIf, self).__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts, args):
+        we_are_present = self.name in opts
+        other_present = self.not_required_if in opts
+
+        if other_present:
+            if we_are_present:
+                raise click.UsageError(
+                    "Illegal usage: `%s` is mutually exclusive with `%s`." % (
+                        self.name, self.not_required_if))
+            else:
+                self.prompt = None
+
+        return super(NotRequiredIf, self).handle_parse_result(
+            ctx, opts, args)
