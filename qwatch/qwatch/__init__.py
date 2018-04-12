27bb8a65bdde58d75a552ee2172916e86e335136
RAG-home <rgilmore@umc.edu>
2018-04-11 23:40:14 -0500

    Abstracted the yaml setup for OrderedDicts.

--- qwatch/qwatch/__init__.py
+++ qwatch/qwatch/__init__.py
@@ -1,9 +1,20 @@
 __version__ = '0.1.0'
 from .core import Qwatch, NotRequiredIf
 from .cli import qwatch
+
+
+def represent_dictionary_order(cls, dict_data):
+    return cls.represent_mapping('tag:yaml.org,2002:map', dict_data.items())
+
+
+def _setup_yaml():
+    """ https://stackoverflow.com/a/8661021 """
+    yaml.add_representer(OrderedDict, represent_dictionary_order)
+
 __all__ = (
     "Qwatch",
     "NotRequiredIf",
     "utils",
     "qwatch",
+    "_setup_yaml",
            )
